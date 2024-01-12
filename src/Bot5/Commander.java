31078ea1e6cf8a078dc525bec10cb26bd85a3b2f
HugoIngelsson <cingelsson@ucsd.edu>
2024-01-12 14:31:55 -0800

    Started work on macro
    Commander.java now figures out the potentially weakest enemy flag and tells all units to attack it. Also made some slight changes to units to make them less likely to bump into each other when following the flag carrier.

---
+++ src/Bot5/Commander.java
@@ -0,0 +1,204 @@
+package Bot5;
+
+import battlecode.common.GameActionException;
+import battlecode.common.GlobalUpgrade;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class Commander extends Robot {
+    RobotController rc;
+    int id;
+
+    int[] flagDefensibility;
+    MapLocation[] flags;
+
+    public Commander(RobotController rc, int id) throws GameActionException {
+        super(rc, id);
+
+        this.rc = rc;
+        this.id = id;
+
+        findSymmetry();
+        readProtection();
+        getFlagSafeties();
+    }
+
+    void play() throws GameActionException {
+        // do some kind of big brain stuff or smth idk
+    }
+
+    @Override
+    void initTurn() throws GameActionException {
+        super.initTurn();
+
+        if (rc.getRoundNum() == 751) rc.buyGlobal(GlobalUpgrade.ACTION);
+        else if (rc.getRoundNum() == 1501) rc.buyGlobal(GlobalUpgrade.HEALING);
+    }
+
+    @Override
+    void endTurn() throws GameActionException {
+        if (rc.getRoundNum() == 200) {
+            int byteZero = rc.readSharedArray(0);
+            byteZero -= 0x1c00;
+            rc.writeSharedArray(0, byteZero);
+        }
+    }
+
+    public void getFlagSafeties() throws GameActionException {
+        int byteZero = rc.readSharedArray(0);
+
+        MapLocation[] enemyFlags = new MapLocation[3];
+        if (!RobotPlayer.bitAt(byteZero, 15)) {
+            enemyFlags[0] = reflectVertically(flags[0]);
+            enemyFlags[1] = reflectVertically(flags[1]);
+            enemyFlags[2] = reflectVertically(flags[2]);
+        }
+        else if (!RobotPlayer.bitAt(byteZero, 14)) {
+            enemyFlags[0] = reflectHorizontally(flags[0]);
+            enemyFlags[1] = reflectHorizontally(flags[1]);
+            enemyFlags[2] = reflectHorizontally(flags[2]);
+        }
+        else {
+            enemyFlags[0] = rotate(flags[0]);
+            enemyFlags[1] = rotate(flags[1]);
+            enemyFlags[2] = rotate(flags[2]);
+        }
+
+        MapLocation weakestSpot = null;
+        int defensibility = Integer.MAX_VALUE;
+        for (int i=0; i<3; i++) {
+            int minSafe = enemyFlags[i].distanceSquaredTo(new MapLocation(rc.getMapWidth()/2, rc.getMapHeight()/2));
+            int minHazard = Integer.MAX_VALUE;
+            for (int j=0; j<3; j++) {
+                minHazard = Math.min(minHazard, enemyFlags[i].distanceSquaredTo(flags[j]));
+            }
+
+            flagDefensibility[i] *= 10;
+            flagDefensibility[i] += minHazard + minSafe;
+
+            if (flagDefensibility[i] < defensibility) {
+                weakestSpot = enemyFlags[i];
+                defensibility = flagDefensibility[i];
+            }
+        }
+
+        rc.writeSharedArray(4, MapHelper.poseEncoder(weakestSpot));
+        rc.writeSharedArray(5, MapHelper.poseEncoder(weakestSpot));
+        rc.writeSharedArray(6, MapHelper.poseEncoder(weakestSpot));
+
+        MapLocation closestSpawn = null;
+        int distance = Integer.MAX_VALUE;
+        for (int i=0; i<3; i++) {
+            if (distance > weakestSpot.distanceSquaredTo(flags[i])) {
+                distance = weakestSpot.distanceSquaredTo(flags[i]);
+                closestSpawn = flags[i];
+            }
+        }
+
+        int halfwayToTheMiddle = MapHelper.poseEncoder(new MapLocation(
+                                        (closestSpawn.x + rc.getMapWidth()) / 2,
+                                        (closestSpawn.y + rc.getMapHeight()) / 2
+                                 ));
+
+        rc.writeSharedArray(7, MapHelper.poseEncoder(weakestSpot));
+        rc.writeSharedArray(8, MapHelper.poseEncoder(weakestSpot));
+        rc.writeSharedArray(9, MapHelper.poseEncoder(weakestSpot));
+
+        // turn on recon bits
+        byteZero |= 0x1c00;
+        rc.writeSharedArray(0, byteZero);
+    }
+
+    private void readProtection() throws GameActionException {
+        flagDefensibility = new int[3];
+        flagDefensibility[0] = rc.readSharedArray(10);
+        flagDefensibility[1] = rc.readSharedArray(11);
+        flagDefensibility[2] = rc.readSharedArray(12);
+
+        rc.writeSharedArray(10, 0);
+        rc.writeSharedArray(11, 0);
+        rc.writeSharedArray(12, 0);
+    }
+
+    private void findSymmetry() throws GameActionException {
+        int write = rc.readSharedArray(0);
+        MapLocation[] announced = rc.senseBroadcastFlagLocations();
+
+        flags = new MapLocation[3];
+        flags[0] = MapHelper.poseDecoder(rc.readSharedArray(1));
+        flags[1] = MapHelper.poseDecoder(rc.readSharedArray(2));
+        flags[2] = MapHelper.poseDecoder(rc.readSharedArray(3));
+
+        if (checkTripose(flags, announced, 0, 0, 1, 2) &&
+                checkTripose(flags, announced, 0, 0, 2, 1) &&
+                checkTripose(flags, announced, 0, 1, 0, 2) &&
+                checkTripose(flags, announced, 0, 1, 2, 0) &&
+                checkTripose(flags, announced, 0, 2, 0, 1) &&
+                checkTripose(flags, announced, 0, 2, 1, 0)) {
+            write |= 0x8000;
+        }
+        if (checkTripose(flags, announced, 1, 0, 1, 2) &&
+                checkTripose(flags, announced, 1, 0, 2, 1) &&
+                checkTripose(flags, announced, 1, 1, 0, 2) &&
+                checkTripose(flags, announced, 1, 1, 2, 0) &&
+                checkTripose(flags, announced, 1, 2, 0, 1) &&
+                checkTripose(flags, announced, 1, 2, 1, 0)) {
+            write |= 0x4000;
+        }
+        if (checkTripose(flags, announced, 2, 0, 1, 2) &&
+                checkTripose(flags, announced, 2, 0, 2, 1) &&
+                checkTripose(flags, announced, 2, 1, 0, 2) &&
+                checkTripose(flags, announced, 2, 1, 2, 0) &&
+                checkTripose(flags, announced, 2, 2, 0, 1) &&
+                checkTripose(flags, announced, 2, 2, 1, 0)) {
+            write |= 0x2000;
+        }
+
+        rc.writeSharedArray(0, write);
+    }
+
+    // checks whether the matching flag-comparisons work with the given symmetry
+    private boolean checkTripose(MapLocation[] flags, MapLocation[] announced, int check, int _0, int _1, int _2) {
+        MapLocation[] modulated = {null, null, null};
+
+        switch (check) {
+            case 0: { // vertical
+                modulated[0] = reflectVertically(flags[0]);
+                modulated[1] = reflectVertically(flags[1]);
+                modulated[2] = reflectVertically(flags[2]);
+                break;
+            }
+            case 1: { // horizontal
+                modulated[0] = reflectHorizontally(flags[0]);
+                modulated[1] = reflectHorizontally(flags[1]);
+                modulated[2] = reflectHorizontally(flags[2]);
+                break;
+            }
+            default: { // rotational
+                modulated[0] = rotate(flags[0]);
+                modulated[1] = rotate(flags[1]);
+                modulated[2] = rotate(flags[2]);
+                break;
+            }
+        }
+
+        return !announced[_0].isWithinDistanceSquared(modulated[0], 100) ||
+               !announced[_1].isWithinDistanceSquared(modulated[1], 100) ||
+               !announced[_2].isWithinDistanceSquared(modulated[2], 100);
+    }
+
+    private MapLocation reflectVertically(MapLocation original) {
+        return new MapLocation(original.x, rc.getMapHeight() - original.y - 1);
+    }
+
+    private MapLocation reflectHorizontally(MapLocation original) {
+        return new MapLocation(rc.getMapWidth() - original.x - 1, original.y);
+    }
+
+    private MapLocation rotate(MapLocation original) {
+        return new MapLocation(rc.getMapWidth() - original.x - 1, rc.getMapHeight() - original.y - 1);
+    }
+}
---
+++ src/Bot5/Robot.java
@@ -0,0 +1,157 @@
+package Bot5;
+
+import Bot5.fast.FastMath;
+import battlecode.common.*;
+
+import java.util.Arrays;
+
+public abstract class Robot {
+    RobotController rc;
+    int id;
+    int byteZero;
+    boolean justSpawned;
+    MapLocation curDest;
+    MapLocation tempTarget;
+
+    Team myTeam;
+    Team enemyTeam;
+
+    RobotInfo[] nearbyEnemies;
+    RobotInfo[] nearbyAllies;
+    MapLocation[] nearbyCrumbs;
+    FlagInfo[] enemyFlags;
+
+    public Robot(RobotController rc, int id) throws GameActionException {
+        this.rc = rc;
+        this.id = id;
+
+        this.myTeam = rc.getTeam();
+        switch (this.myTeam) {
+            case A: this.enemyTeam = Team.B; break;
+            case B: this.enemyTeam = Team.A; break;
+        }
+
+        FastMath.initRand(rc);
+    }
+
+    abstract void play() throws GameActionException;
+
+    void initTurn() throws GameActionException {
+        this.nearbyEnemies = rc.senseNearbyRobots(-1, this.enemyTeam);
+        this.nearbyAllies = rc.senseNearbyRobots(-1, this.myTeam);
+        this.nearbyCrumbs = rc.senseNearbyCrumbs(-1);
+        this.enemyFlags = rc.senseNearbyFlags(-1, this.enemyTeam);
+        this.byteZero = rc.readSharedArray(0);
+
+        if (this.justSpawned && RobotPlayer.bitAt(this.byteZero, 12-id%3)) {
+            curDest = MapHelper.poseDecoder(rc.readSharedArray(7+id%3));
+        }
+        else {
+            curDest = MapHelper.poseDecoder(rc.readSharedArray(4+id%3));
+            this.justSpawned = false;
+        }
+
+        PathFinding.initTurn();
+    }
+
+    void endTurn() throws GameActionException {
+        if (tempTarget != null && rc.getLocation().distanceSquaredTo(tempTarget) <= 4) tempTarget = null;
+    }
+
+    void playIfUnspawned() throws GameActionException {
+        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+
+        // gets the next open spot based on the duck's position in the turn order
+        int nextOpenSpawn = getNextSpawnableLocation(spawnLocs, id%27);
+
+        // if nextOpenSpawn == -1, then there are no spawnable positions
+        if (nextOpenSpawn != -1) {
+            rc.spawn(spawnLocs[nextOpenSpawn]);
+            this.justSpawned = true;
+
+            atSpawnActions();
+        }
+    }
+
+    void atSpawnActions() throws GameActionException {
+        // update death counter for my group
+        int byteThirteen = rc.readSharedArray(13);
+        System.out.println(byteThirteen);
+        byteThirteen += 1<<(id%3*5);
+        rc.writeSharedArray(13, byteThirteen);
+
+        // move away from the flag to clear out space
+        FlagInfo[] closeFlags = rc.senseNearbyFlags(0);
+        if (rc.getRoundNum() == 1) {
+            System.out.println(id + " " + closeFlags.length);
+            if (closeFlags.length > 0) {
+                if (rc.readSharedArray(1) == 0) {
+                    rc.writeSharedArray(1, MapHelper.poseEncoder(rc.getLocation()));
+
+                    int impassables = 0;
+                    MapInfo[] mi = rc.senseNearbyMapInfos(-1);
+                    for (MapInfo mii : mi) if (!mii.isPassable()) impassables++;
+                    rc.writeSharedArray(10, impassables);
+                }
+                else if (rc.readSharedArray(2) == 0) {
+                    rc.writeSharedArray(2, MapHelper.poseEncoder(rc.getLocation()));
+
+                    int impassables = 0;
+                    MapInfo[] mi = rc.senseNearbyMapInfos(-1);
+                    for (MapInfo mii : mi) if (!mii.isPassable()) impassables++;
+                    rc.writeSharedArray(11, impassables);
+                }
+                else {
+                    rc.writeSharedArray(3, MapHelper.poseEncoder(rc.getLocation()));
+
+                    int impassables = 0;
+                    MapInfo[] mi = rc.senseNearbyMapInfos(-1);
+                    for (MapInfo mii : mi) if (!mii.isPassable()) impassables++;
+                    rc.writeSharedArray(12, impassables);
+                }
+            }
+            else {
+                closeFlags = rc.senseNearbyFlags(2);
+                if (rc.isMovementReady() && rc.canMove(closeFlags[0].getLocation().directionTo(rc.getLocation())))
+                    rc.move(closeFlags[0].getLocation().directionTo(rc.getLocation()));
+            }
+        }
+        else {
+            // do something
+        }
+    }
+
+    public Team getMyTeam() {
+        return this.myTeam;
+    }
+
+    public Team getEnemyTeam() {
+        return this.enemyTeam;
+    }
+
+    public RobotInfo[] getNearbyEnemies() {
+        return this.nearbyEnemies;
+    }
+
+    public RobotInfo[] getNearbyAllies() {
+        return this.nearbyAllies;
+    }
+
+    public MapLocation[] getNearbyCrumbs() {
+        return this.nearbyCrumbs;
+    }
+
+    public FlagInfo[] getEnemyFlags() { return this.enemyFlags; }
+
+    public int getNextSpawnableLocation(MapLocation[] spawns, int id) {
+        for (int i=id; i<spawns.length; i++) {
+            if (rc.canSpawn(spawns[i])) return i;
+        }
+
+        for (int i=id-1; i>=0; i--) {
+            if (rc.canSpawn(spawns[i])) return i;
+        }
+
+        return -1;
+    }
+}
